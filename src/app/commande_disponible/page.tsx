9fe64f7c50c6e08cd41db72ae55111fd06a84529
Nicolas FOUQUE <nicolas.fouque@laposte.net>
2024-06-13 09:55:03 +0200

    add comment for table

--- src/app/commande_disponible/page.tsx
+++ src/app/commande_disponible/page.tsx
@@ -15,12 +15,12 @@ const INITIAL_VISIBLE_COLUMNS = [
   "adresse_resto",
   "adresse_client",
   "payment_method",
-  "actions",
+  "actions",//Always here
 ];
 
 const items = data.map((item) => (
   {
-    id: item.order_id,
+    id: item.order_id,//We always need an unique id, but it is never shown. Make sure to used an unique key as value.
     adresse_resto: item.restaurant.address.street + ', ' + item.restaurant.address.city,
     ville_resto: item.restaurant.address.city,
     adresse_client: item.customer.address.street + ', ' + item.customer.address.city,
@@ -35,16 +35,16 @@ const columns = [
   { name: "Adresse du client", uid: "adresse_client" },
   { name: "Ville du client", uid: "ville_client" },
   { name: "Moyen de paiement", uid: "payment_method" },
-  { name: "Actions", uid: "actions" },
+  { name: "Actions", uid: "actions" },//Always here
 ];
 
 const options: Options = {
-  content: "commande(s) trouvée(s)", //Affiche pour le nombre d'items et s'il n'y pas d'items trouvé
-  search_name: "Chercher par ville", //Titre de la barre de recherche
-  search_uid: ["ville_resto", "ville_client"], //uid des colonnes à filtrer
-  selection_mode: "none", //"none", "single" ou "multiple"
+  content: "commande(s) trouvée(s)", //Used for the number of items and when no items was found
+  search_name: "Chercher par ville", //Title of the search bar
+  search_uid: ["ville_resto", "ville_client"], //ALWAYS AN ARRAY, uid of the column to filter 
+  selection_mode: "none", //"none", "single" or "multiple"
   option_name: "Moyen de paiement", //Name of the option filter
-  option_uid: "payment_method", //uid of the column filtered with the option
+  option_uid: "payment_method", //ALWAYS A SINGLE STRING, uid of the column filtered with the option
   value_option: [
     { name: "Carte de crédit", uid: "Carte de crédit" }, // uid need to be exactly the same as item's value. Name is the string to be printed
     { name: "Liquide", uid: "Liquide" },
