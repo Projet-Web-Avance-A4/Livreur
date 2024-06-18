3dfec018aede49275efae707b85b11b4ecdd5425
Arthur Joye <arthurjoye0701@gmail.com>
2024-06-18 11:15:40 +0200

    remove unused NextUIProvider

--- src/app/components/portalCard/portalCard.tsx
+++ src/app/components/portalCard/portalCard.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Button } from "@nextui-org/button";
-import { NextUIProvider } from "@nextui-org/react";
 import { Card, CardBody } from "@nextui-org/card";
 import { iPortalCard } from "@/app/interfaces/card";
 import { handleRedirect } from './utils';
